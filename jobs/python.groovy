62cc1917b8caff1a5cdffd50bc253fbc0f3e062e
dcmikki <dcmikki@gmail.com>
2018-01-16 19:59:06 +0000

    python.groovy

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -4,23 +4,21 @@ repo_name = "repo"
 
 
 pipelineJob(project_name) {
-	definition {
-		triggers{
-			scm('H/1 * * * *')
-		}
-		
-		spsScm{
-			scm {
-				git {
-					remote {
-					name(repo_name)
-					url(repo)
-					}
-				}
-				scriptPath("Jenkinsfile")
-			
-			}
-		}
-	}
+    definition {
+        triggers {
+            scm('H/1 * * * *')
+        }
+        spsScm {
+            scm {
+                git {
+                    remote {
+                    name(repo_name)
+                    url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
 }
 
