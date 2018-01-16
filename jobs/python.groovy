e81546cd58f9506cb28a2198b06eb79a4b4edf97
dcmikki <dcmikki@gmail.com>
2018-01-16 20:54:40 +0000

    python.groovy mis cambios

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -1,6 +1,6 @@
 project_name = "jenkins-pipeline-python"
 repo = "git@github.com:dcmikki/jenkins-holamundo.git"
-repo_name = "repo"
+repo_name = "master"
 
 pipelineJob(project_name) {
     definition {
